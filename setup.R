34065df26bba49fbe3173f6685f25e7275024d28
Blas <blasbenito@gmail.com>
2021-11-14 22:07:36 +0100

    first commit

---
+++ setup.R
@@ -0,0 +1,6 @@
+#installing xaringan
+remotes::install_github("yihui/xaringan")
+library(xaringan)
+install.packages("xaringanthemer")
+remotes::install_github("gadenbuie/xaringanExtra")
+library(xaringanExtra)
